b1cf3c1d74c5d06908c36491020494c9c74b53ad
Roman Kirillov <sigizmund@gmail.com>
2020-07-26 13:21:43 +0100

    Entire story is downloaded OK

--- src/main/kt/Main.kt
+++ src/main/kt/Main.kt
@@ -1,11 +1,9 @@
 import com.google.common.io.Files
-import org.openqa.selenium.By
-import org.openqa.selenium.Keys
+import org.openqa.selenium.*
 import org.openqa.selenium.NoSuchElementException
-import org.openqa.selenium.WebDriver
-import org.openqa.selenium.WebElement
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
+import org.openqa.selenium.interactions.Actions
 import org.openqa.selenium.remote.RemoteWebDriver
 import org.openqa.selenium.support.ui.ExpectedConditions
 import org.openqa.selenium.support.ui.WebDriverWait
@@ -24,6 +22,8 @@ object Main {
     val STORY_URL = "https://author.today/reader/45969/361004"
     val logger = Logger.getLogger("Main")
 
+    val TITLE_CSS_SELECTOR = "#text-container > h1"
+
 
     val next =     "#reader > div.reader-content.hidden-print > div > ul > li.next > a"
 
@@ -39,10 +39,14 @@ object Main {
         )
     }
 
+    fun loaded(driver: RemoteWebDriver): Boolean {
+        val res = driver.executeScript("return document.readyState")
+        return res.equals("complete");
+    }
+
     private fun main() {
         val props = Properties()
         props.load(FileInputStream("login.properties"))
-
 
         val chromeDriverPath = "chromedriver/chromedriver.exe"
         System.setProperty("webdriver.chrome.driver", chromeDriverPath)
@@ -56,8 +60,6 @@ object Main {
         val LOGIN_XPATH = "/html/body/header/div/nav/ul/li[2]/a"
         waitFor(wait, LOGIN_XPATH)
         driver.findElement(By.ByXPath(LOGIN_XPATH)).click()
-        //        Thread.sleep(1000)
-
         val LOGIN_FIELD_XPATH = "/html/body/div[3]/div/div/div[2]/div/div/div/form/div[1]/input"
         waitFor(wait, LOGIN_FIELD_XPATH)
         val login = driver.findElement(By.ByXPath(LOGIN_FIELD_XPATH))
@@ -75,9 +77,9 @@ object Main {
             extractChapter(driver)
             try {
                 val element = driver.findElement(By.ByCssSelector(next))
+                (driver as JavascriptExecutor).executeScript("arguments[0].scrollIntoView(true);", element)
                 element.click()
                 Thread.sleep(100)
-                waitFor(wait, "/html/body/div[1]/section/div[2]/div")
             } catch (e: NoSuchElementException) {
                 break
             }
@@ -88,38 +90,57 @@ object Main {
     }
 
     private fun extractChapter(driver: WebDriver) {
-        val elements = driver.findElements(By.cssSelector("div.text-container > *"))
-        var text = ""
-        for (element in elements) {
-            logger.info(element.getAttribute("innerHTML"))
+        logger.info("Url: ${driver.currentUrl}")
+        val wait = WebDriverWait(driver, 10)
 
+        waitFor(wait, "/html/body/div[1]/section/div[2]/div")
+        try {
+            wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(next)))
+        } catch (e: TimeoutException) {
+            logger.warning("Timeout exception, possibly last page, proceeding at own risk")
+        }
+
+        wait.until {
+            loaded(driver as RemoteWebDriver)
+        }
+
+        waitForCss(wait, TITLE_CSS_SELECTOR)
+        val title = driver.findElement(By.cssSelector(TITLE_CSS_SELECTOR)).getAttribute("innerText")
+        val elements = driver.findElements(By.cssSelector("div.text-container > *"))
+        var text = "<!-- ${driver.currentUrl} -->\n"
+        text += "<h1>$title</h1>\n"
+        for (element in elements) {
             val images = element.findElements(By.cssSelector("img"))
             for (img in images) {
                 val src = img.getAttribute("src")
-                logger.info("Image: %s".format(src))
                 val url = URL(src)
 
                 val bi = ImageIO.read(url)
                 val outFile = File("./" + url.file)
                 Files.createParentDirs(outFile)
                 ImageIO.write(bi, "jpg", outFile)
-                logger.info("Created %s".format(outFile.absolutePath))
+                logger.info("Created ${outFile.absolutePath}")
 
                 setAttribute(driver as RemoteWebDriver, img, "src", outFile.path)
             }
 
             var innerHTML = element.getAttribute("innerHTML")
-
-            text = text + "<div>" + innerHTML + "</div>"
+            text = "$text<div>$innerHTML</div>\n"
         }
 
-        val result = File("./" + driver.title + ".html")
-        BufferedWriter(FileWriter(result)).write(text)
+        val result = File("./$title.html")
+        val bufferedWriter = BufferedWriter(FileWriter(result))
+        bufferedWriter.write(text)
+        bufferedWriter.flush()
+        bufferedWriter.close()
     }
 
     private fun waitFor(wait: WebDriverWait, xpath: String) {
         wait.until(ExpectedConditions.elementToBeClickable(By.ByXPath(xpath)))
     }
 
+    private fun waitForCss(wait: WebDriverWait, cssSelector: String) {
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(cssSelector)))
+    }
 
 }
