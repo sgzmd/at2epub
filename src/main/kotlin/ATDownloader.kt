465d82a3ab7a2c9e1cf3f9b4d18c19144af09da3
Roman Kirillov <sigizmund@gmail.com>
2020-07-27 08:43:51 +0100

    Command line

--- src/main/kotlin/ATDownloader.kt
+++ src/main/kotlin/ATDownloader.kt
@@ -1,16 +1,24 @@
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.option
 import java.io.FileInputStream
 import java.util.*
+
+internal class Downloader : CliktCommand() {
+    val url: String by option(help = "First URL of the story in reading mode").default("")
+    val config: String by option(help = "Config file to use").default("login.properties")
+    override fun run() {
+        val props = Properties()
+        props.load(FileInputStream(config))
+        val downloader = StoryDownloader(props)
+        assert(downloader.downloadStory(url) > 0)
+        downloader.writeFiles()
+    }
+}
 
 class ATDownloader {
     companion object {
         @JvmStatic
-        fun main(args: Array<String>) {
-            val url = args[0]
-            val props = Properties()
-            props.load(FileInputStream("login.properties"))
-            val downloader = StoryDownloader(props)
-            assert(downloader.downloadStory(url) > 0)
-            downloader.writeFiles()
-        }
+        fun main(args: Array<String>) = Downloader().main(args)
     }
 }
